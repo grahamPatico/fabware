4e4149abe0485ab12688fff2cc36be0623c314de
grahamPatico <68247276+grahamPatico@users.noreply.github.com>
2026-04-27 19:11:47 -0500

    chore: drop dead Drizzle/api-spec libs (Convex is the only backend)

--- lib/db/src/schema/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./projects";
-export * from "./messages";
-export * from "./partSpecs";
-export * from "./partRevisions";
-export * from "./materials";
-export * from "./assemblyParts";
-export * from "./waitlist";
